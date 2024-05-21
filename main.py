9d96af8a28070303acb629a86dc30b7c6962f26f
ENNARSH <gennaro.liguori.sh@gmail.com>
2024-05-21 12:18:56 +0200

    added args

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import random
 import time
 import requests
-
-be_url = "http://localhost:8080"
-be_rooms_url = f"{be_url}/rooms"
-influx_url = 'http://localhost:8086'
-influx_username = 'admin'
-influx_password = 'password'
+import argparse
 
 
 def get_rooms_from_be(url_endpoint_rooms):
-    response = requests.get(url_endpoint_rooms)
+    response = requests.get(url_endpoint_rooms + "/rooms")
     data = response.json()
     return data
 
 
-def process_data_be():
+def process_data_be(be_rooms_url, influx_url):
     rooms_data = get_rooms_from_be(be_rooms_url)
     for room in rooms_data:
         current_time = int(round(time.time() * 1000000000))
@@ -25,12 +20,11 @@ def process_data_be():
         humidity = random.randint(0, 100)
         temperature = random.randint(-10, 40)
         influx_point = build_influx_point(measurement, temperature, humidity, current_time)
-        write_data(room["usernameID"], retention, influx_point)
+        write_data(room["usernameID"], retention, influx_point, influx_url)
 
 
-def write_data(database, retention, influx_point):
+def write_data(database, retention, influx_point, influx_url):
     influx_write_url = f"{influx_url}/write?db={database}&rp={retention}"
-
     response = requests.post(influx_write_url, data=influx_point)
     if response.status_code != 204:
         print(f"Failed to write data to InfluxDB: {response.content}")
@@ -40,4 +34,10 @@ def build_influx_point(measurement, temperature, humidity, current_time):
     return f"{measurement} temperature={temperature},humidity={humidity} {current_time}"
 
 
-process_data_be()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process data from API and send to InfluxDB.')
+    parser.add_argument('be_url', type=str)
+    parser.add_argument("-i", '--influx_url', type=str, help='URL of the InfluxDB endpoint', required=True)
+    args = vars(parser.parse_args())
+
+    process_data_be(args['be_url'], args['influx_url'])
